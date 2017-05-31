4b5f015674a2c6d5b6f586051f437e8be50a1409
vla4253 <vla4253@gmail.com>
2017-05-31 11:26:29 +0200

    Removing unnecessary lines

--- app/src/main/java/com/example/vladyslav/wydatkikontrasumienie/MainActivity.java
+++ app/src/main/java/com/example/vladyslav/wydatkikontrasumienie/MainActivity.java
@@ -384,9 +384,6 @@ class PurchaseDataBase extends SQLiteOpenHelper {
         Calendar calendar = Calendar.getInstance();
         int currentDay = calendar.get(Calendar.DAY_OF_MONTH);
         int currentMonth = (calendar.get(Calendar.MONTH))+1;
-        System.out.println("---!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!---1");
-        System.out.println(currentMonth);
-        System.out.println("---!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!---1");
         int currentYear = calendar.get(Calendar.YEAR);
         if(period.equals("week")) {
             if((currentDay - 6) < 1) {
